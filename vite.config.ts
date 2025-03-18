e9edd60a5e7c578f4ad333ac0edcf66e2fc2d6f1
devnoman43 <dev.mywok@gmail.com>
2025-03-19 04:20:09 +0500

    files updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import {defineConfig} from 'vite';
-import {hydrogen} from '@shopify/hydrogen/vite';
-import {oxygen} from '@shopify/mini-oxygen/vite';
-import {vitePlugin as remix} from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import { hydrogen } from '@shopify/hydrogen/vite';
+import { oxygen } from '@shopify/mini-oxygen/vite';
+import { vitePlugin as remix } from '@remix-run/dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -29,23 +29,12 @@ export default defineConfig({
     tsconfigPaths(),
   ],
   build: {
-    // Allow a strict Content-Security-Policy
-    // withtout inlining assets as base64:
-    assetsInlineLimit: 0,
+    outDir: 'dist', // Explicitly set the output directory
+    assetsInlineLimit: 0, // Allow strict Content-Security-Policy
   },
   ssr: {
     optimizeDeps: {
-      /**
-       * Include dependencies here if they throw CJS<>ESM errors.
-       * For example, for the following error:
-       *
-       * > ReferenceError: module is not defined
-       * >   at /Users/.../node_modules/example-dep/index.js:1:1
-       *
-       * Include 'example-dep' in the array below.
-       * @see https://vitejs.dev/config/dep-optimization-options
-       */
-      include: [],
+      include: [], // Add dependencies here if they throw CJS<>ESM errors
     },
   },
 });
